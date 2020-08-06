53f07dc994340d12abea41272a4c9a60718b2cb8
jivi3 <jivirockz1@gmail.com>
2020-08-06 18:32:44 -0400

    quick navbar fix

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
-        document.getElementById("main-nav").style.padding = "1em 0 0 1em";
+        document.getElementById("main-nav").style.padding = "1em 0 0 0";
         document.getElementById("main-nav").style.background = "#1A0063d9";
-        document.getElementById("main-nav").style.height = "5em";
+        document.getElementById("main-nav").style.height = "6em";
 
 
 
