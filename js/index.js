38b21d5ce29561a0cfea802c10504a17cb168aad
jivi3 <jivirockz1@gmail.com>
2020-08-12 15:55:32 -0400

    application form live

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+// $(function() {
+//     $('ul.nav a').bind('click', function(event) {
+//         event.preventDefault();
+//         var $anchor = $(this);
+//         console.log($anchor.attr('href'))
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1000);
+//         event.preventDefault();
+//     });
+// });
+
+
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
@@ -11,6 +24,17 @@ function scrollFunction() {
         document.getElementById("main-nav").style.background = "transparent";
     }
 }
+
+
+
+// $('#main-nav').bind('click', 'ul li a', function(event) {
+//     event.preventDefault();
+//     var location = $(event.target.hash).offset().top;
+//     $('html, body').animate({
+//         scrollTop: location
+//     }, 500);
+// });
+
 
 // init controller
 var controller = new ScrollMagic.Controller();
@@ -30,7 +54,7 @@ for (let i = 0; i < revealElements.length; i++) {
     // create a scene for each element
     new ScrollMagic.Scene({
             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-            offset: 100, // start a little later
+            offset: 20, // start a little later
             triggerHook: 0.9,
         })
         .setClassToggle(revealElements[i], "visible") // add class toggle
