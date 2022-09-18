374bb21aae258188283ce37c31c354d6e65362af
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-09-18 08:54:52 -0400

    responsive tweaks

--- js/index.js
+++ js/index.js
@@ -10,23 +10,20 @@
 //     });
 // });
 
-
-
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
-        document.getElementById("main-nav").style.padding = "1em 0 0 0";
-        document.getElementById("main-nav").style.background = "#1A0063d9";
-        document.getElementById("main-nav").style.height = "6em";
-
-    } else {
-        document.getElementById("main-nav").style.padding = "3em 0 0 0";
-        document.getElementById("main-nav").style.background = "transparent";
-    }
+  if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+    document.getElementById('main-nav').style.padding = '1em 0 0 0';
+    document.getElementById('main-nav').style.background = '#1A0063d9';
+    document.getElementById('main-nav').style.height = '10em';
+  } else {
+    document.getElementById('main-nav').style.padding = '3em 0 0 0';
+    document.getElementById('main-nav').style.background = 'transparent';
+  }
 }
-
-
 
 // $('#main-nav').bind('click', 'ul li a', function(event) {
 //     event.preventDefault();
@@ -36,29 +33,25 @@ function scrollFunction() {
 //     }, 500);
 // });
 
-
 // init controller
 var controller = new ScrollMagic.Controller();
 
-
-var revealElements = document.getElementsByClassName("faq-card");
+var revealElements = document.getElementsByClassName('faq-card');
 for (let i = 0; i < revealElements.length; i++) {
-    // create a scene for each element
-    new ScrollMagic.Scene({
-            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-            offset: 20, // start a little later
-            triggerHook: 0.9,
-        })
-        .setClassToggle(revealElements[i], "visible") // add class toggle
-        .addTo(controller);
-
+  // create a scene for each element
+  new ScrollMagic.Scene({
+    triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+    offset: 20, // start a little later
+    triggerHook: 0.9,
+  })
+    .setClassToggle(revealElements[i], 'visible') // add class toggle
+    .addTo(controller);
 }
 
+document.getElementById('signup-button').onclick = function () {
+  location.href = 'https://forms.gle/Coao6o8kDYFhscdb6';
+};
 
-document.getElementById("signup-button").onclick = function() {
-    location.href = "https://forms.gle/Coao6o8kDYFhscdb6";
-}
-
-document.getElementById("discord-button").onclick = function() {
-    location.href = "https://discord.gg/8HAVggeyMv";
-}
+document.getElementById('discord-button').onclick = function () {
+  location.href = 'https://discord.gg/8HAVggeyMv';
+};
